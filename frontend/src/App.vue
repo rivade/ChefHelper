7084da185d782a6cf10daafc3ae5fb7995ea3e11
rivade <gdrivade@gmail.com>
2026-09-21 13:46:18 +0200

    Created header in app.vue to be displayed on every page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,40 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import logo from "./assets/logo.svg";
+const route = useRoute();
+</script>
+
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/signup">Sign up</RouterLink>
-    <RouterLink to="/login">Log in</RouterLink>
-    <RouterLink to="/dashboard">Dashboard</RouterLink>
-    <RouterLink to="/anonrecipebrowsing">Recept</RouterLink>
-  </nav>
+  <header class="h-[72px] bg-[#EFEFEB] p-3 font-['Roboto']">
+    <nav class="flex h-full items-center justify-between">
+      <RouterLink
+        to="/"
+        class="flex items-center gap-3 text-2xl font-extrabold"
+      >
+        <img :src="logo" alt="ChefHelper logo" class="h-12 w-12" />
+        <span>ChefHelper</span>
+      </RouterLink>
+
+      <div class="flex items-center gap-6">
+        <RouterLink
+          v-if="route.path === '/'"
+          to="/anonrecipebrowsing"
+          class="font-semibold underline underline-offset-4"
+          >Recept</RouterLink
+        >
+        <RouterLink to="/" class="font-semibold underline underline-offset-4"
+          >Kontakt</RouterLink
+        >
+        <RouterLink to="/" class="font-semibold underline underline-offset-4"
+          >Om oss</RouterLink
+        >
+      </div>
+    </nav>
+  </header>
+
+  <RouterLink to="/signup">Sign up</RouterLink>
+  <RouterLink to="/login">Log in</RouterLink>
+  <RouterLink to="/dashboard">Dashboard</RouterLink>
 
   <RouterView />
 </template>
